5ec972905438dfb44582f61b09e8d329dd15b1a1
xiaogang00 <872310734@qq.com>
2017-04-09 01:02:41 +0800

    Learn for Deep learning

---
+++ CNN/CNN.py
@@ -0,0 +1,39 @@
+from tensorflow,examples.tutorials.mnist import input_data
+import tensorflow as tf 
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot = True)
+sess = tf.InteractiveSession()
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape = shape)
+    return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides = [1,1,1,1], padding = 'SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1,2,2,1], padding = 'SAME')
+
+
+x = tf.placeholder(tf.float32, [None, 784])
+y = tf.placeholder(tf.float32, [None, 10])
+
+x_image = tf.reshape(x, [-1,28,28,1])
+
+W_conv1 = weight_variable([5,5,1,32])
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+W_conv2 = weight_variable([5,5,32,64])
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+w_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable
