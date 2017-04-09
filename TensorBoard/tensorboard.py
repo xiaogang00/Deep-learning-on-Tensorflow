2ae73d5cbb16e586c7af3b1dd4d0d1349332b278
xiaogang00 <872310734@qq.com>
2017-04-10 01:43:30 +0800

    learn for deep learning

---
+++ TensorBoard/tensorboard.py
@@ -0,0 +1,53 @@
+import tensorflow as tf  
+from tensorflow.examples.tutorials.mnist import input_data
+max_steps = 1000
+learning_rate = 0.001
+dropout = 0.9
+data_dir = 'tem/tensorflow/mnist/input_data'
+log_dir = 'tem/tensorflow/mnist/logs/mnist_with_summaries'
+
+mnist = input_data.read_data_sets（data_dir, one_hot = True)
+sess = tf.InteractiveSession()
+
+with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, [None, 784], name = 'x_input')
+    y_ = tf.placeholder(tf.float32, [None, 10], name = 'y_input')
+
+with tf.name_scope('input_reshape'):
+    image_shaped_input = tf.reshape(x, [-1, 28, 28, 1])
+    tf.summary.image('input', image_shaped_input)
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape = shape)
+    return tf.Variable(initial)
+
+def variable_summaries(var):
+    with tf.name_scope('summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
+
+def nn_layer(input_tensor, input_dim, output_dim, layer_name, act = tf.nn.relu):
+    with tf.name_scope(layer_name):
+        with tf.name_scope('weights'):
+            weights = weight_variable([input_dim, output_dim])
+            variable_summaries(weight_variable)
+        with tf.name_scope('biases'):
+            biases = bias_variable([output_dim])
+            variable_summaries(biases)
+        with tf.name_scope('Wx_plus_b'):
+            preactivate = tf.matmul(input_tensor, weights) + biases
+            tf.summary.histogram('pre_activations', preactivate)
+        activations = act(preactivate, name = 'activations')#在这里使用激活函数
+        tf.summary.histogram('activations', activations)
+        return activations
+
