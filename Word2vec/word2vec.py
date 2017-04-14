db51f689a2f746c670a5d6b6e3d6111792ff22d1
xiaogang00 <872310734@qq.com>
2017-04-15 01:08:19 +0800

    Learn for deep learning

--- Word2vec/word2vec.py
+++ Word2vec/word2vec.py
@@ -8,3 +8,19 @@ import numpy as np
 import urllib
 import tensorflow as tf  
 
+url = 'http://mattmahoney.net/dc/'
+
+def maybe_download(filename, expected_bytes):
+    if not os.path.exists(filename):
+        filename, _ = urllib.request.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print ('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to vertify' + filename +' .can you get to it with a browser?'
+        )
+    return filename
+
+filename = maybe_download('text8.zip', 31344016)
